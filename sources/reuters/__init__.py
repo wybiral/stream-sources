034046b9ac3dd513384652e1252bd665aa72d5ba
wybiral <davy.wybiral@gmail.com>
2018-03-09 12:07:30 -0600

    moved reuters.top

--- sources/reuters/__init__.py
+++ sources/reuters/__init__.py
@@ -1,1 +1,12 @@
-from . import business, money, politics, science, technology, top, us, world
+from sources.generic import FeedSource
+from . import business, money, politics, science, technology, us, world
+
+
+class Source(FeedSource):
+
+    SOURCE = {
+        'name': 'Reuters (Top News)',
+        'url': 'https://www.reuters.com',
+    }
+
+    FEED_URL = 'http://feeds.reuters.com/reuters/topNews'
