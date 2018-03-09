d57eb249eff85480b382a13f2aa478ca2cb1d0ad
wybiral <davy.wybiral@gmail.com>
2018-03-09 11:29:05 -0600

    move Source

--- sources/__init__.py
+++ sources/__init__.py
@@ -1,12 +1,1 @@
-from stream import Stream
-
-
-class Source:
-
-    def __init__(self, stream=None):
-        if stream is None:
-            stream = Stream()
-        self.stream = stream
-
-    def update(self):
-        raise NotImplementedError
+from .source import Source
