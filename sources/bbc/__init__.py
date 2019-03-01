02201482b52c6133c03058e5d426193eab4ee666
davy <davy.wybiral@gmail.com>
2019-03-01 08:42:31 -0600

    filter sports from bbc

--- sources/bbc/__init__.py
+++ sources/bbc/__init__.py
@@ -9,3 +9,8 @@ class Source(FeedSource):
     }
 
     FEED_URL = 'http://feeds.bbci.co.uk/news/rss.xml'
+
+    def filter(self, entry):
+        if '/sport/' in entry['link']:
+            return False
+        return True
